27621812ad72ab2c08605b4fecb12fa486838479
ebru-oner <z.ebru.acar@gmail.com>
2024-04-11 17:28:01 +0200

    Adds more string exercises.

--- exercises/string_functions.py
+++ exercises/string_functions.py
@@ -1,3 +1,5 @@
+import string
+
 # Print characters from a string that are present at an even index number
 def print_chars_at_even_index(str):
     index = 0
@@ -64,3 +66,90 @@ def append_into_middle(str1, str2):
     mid = length // 2
     return str1[0:mid]+str2+str1[mid:]
 
+# Create a new string made of the first, middle and last characters of each input string
+def create_combination_of_strings(list_of_strings):
+    combination_str = ""
+    for str in list_of_strings:
+        length = len(str)
+        combination_str+= "".join([str[0], str[length//2], str[-1]])
+    return combination_str
+
+# Arrange string characters such that lowercase letter come first
+def make_lowercases_prefix(str):
+    list_u = []
+    list_l = []
+    for c in str:
+        if c.islower():
+            list_l += [c]
+        else:
+            list_u += [c]
+    return "".join(list_l+list_u)
+
+# Count all letter, digits, special characters from a given string
+def group_characters_in_string(a_str):
+    char_count = 0
+    symbol_count = 0
+    number_count = 0
+    for c in a_str:
+        if c.isalpha():
+            char_count += 1
+        elif c.isdigit():
+            number_count += 1
+        else:
+            symbol_count += 1
+    return a_str + " has " + str(char_count) + " chars " + str(number_count) + " digits " + str(symbol_count) + " symbols."
+
+# String characters balance test, check if all characters of s1 are present in s2
+def is_balanced_string(str1, str2):
+    for c in str1:
+        if not c in str2:
+            return False
+    return True
+
+# Count all occurences of all characters within a string
+def count_char_occurrences(str1):
+    char_map = {}
+    for c in str1:
+        if c in char_map:
+            char_map[c] += 1
+        else:
+            char_map[c] = 1
+    return char_map
+            
+# Reverse a given string
+def reverse_string(str1):
+    reversed_string = str1[::-1]
+    return reversed_string
+
+# Find the last position of a given substring
+def get_last_position(str1, substr):
+    return str1.rfind(substr)
+
+# Split a string with given char
+def split_string(str1, split_char):
+    return str1.split(split_char)
+
+# Remove empty strings from a list of strings
+def remove_empty_strings(str_list):
+    new_list = list(filter(None, str_list))
+    return new_list
+
+# Remove all characters except integers 
+def remove_characters_keep_integers(str1):
+    new_list = [item for item in str1 if item.isdigit()]
+    return "".join(new_list)
+
+# Find words with both alphabets and numbers
+def find_combination_of_alphabets_numbers(str1):
+    str1_list = str1.split()
+    result = []
+    for s in str1_list:
+        if any(c.isalpha() for c in s) and any(c.isdigit() for c in s):
+            result += [s]
+    return result
+
+# Replace each symbol with given char
+def replace_symbols(str1, replace_char):
+    for c in string.punctuation:
+        str1 = str1.replace(c, replace_char)
+    return str1
--- exercises/test_string_functions.py
+++ exercises/test_string_functions.py
@@ -67,7 +67,113 @@ class TestMathFunctions(unittest.TestCase):
         # Test case 2: 1 char string:p
         result = get_middle_three_chars("p")
         self.assertEqual(result, None)
-        
+
+    def test_create_combination_of_strings(self):
+        # Test case 1: basic list:["multiple","strings"]
+        result = create_combination_of_strings(["multiple", "strings"])
+        self.assertEqual(result, "miesis") 
+
+        # Test case 2: empty list:[]
+        result = create_combination_of_strings([])
+        self.assertEqual(len(result), 0) 
+    
+    def test_make_lowercases_prefix(self):
+        # Test case 1: basic str:"HelLo"
+        result = make_lowercases_prefix("HelLo")
+        self.assertEqual(result, "eloHL")
+
+        # Test case 2: empty str:""
+        result = make_lowercases_prefix("")
+        self.assertEqual(len(result), 0) 
+
+    def test_group_characters_in_string(self):
+        # Test case 1: a string with all str: @#$123qwe
+        result = group_characters_in_string("@12qwe")
+        self.assertEqual(result, "@12qwe has 3 chars 2 digits 1 symbols.")
+
+        # Test case 2: empoty string str: ""
+        result = group_characters_in_string("")
+        self.assertEqual(result, " has 0 chars 0 digits 0 symbols.")
+    
+    def test_is_balanced_string(self):
+        # Test case 1: balanced string with all str1: this string, str2:is in this string
+        result = is_balanced_string("this string", "is in this string")
+        self.assertTrue(result)
+
+        # Test case 2: not balanced str1: this string, str2:is not in this
+        result = is_balanced_string("this string","is not in this")
+        self.assertFalse(result)
+
+    def test_count_char_occurrences(self):
+        # Test case 1: empty str:""
+        result = count_char_occurrences("")
+        self.assertEqual(result, {})
+
+        # Test case 2: multiple occurences str:ewrbDDDerweee
+        result = count_char_occurrences("ewrbDDDerweee")
+        self.assertEqual(len(result), 5)
+
+    def test_reverse_string(self):
+        # Test case 1:  empty str=""
+        result = reverse_string("")
+        self.assertEqual(result, "")
+
+        # Test case 2:  basic str="string"
+        result = reverse_string("string")
+        self.assertEqual(result, "gnirts")
+
+    def test_remove_empty_strings(self):
+        # Test case 1: list with empty str, list:["ww","",None,"","dd"]
+        result = remove_empty_strings(["ww","",None,"","dd"])
+        self.assertEqual(result, ["ww","dd"])
+
+        # Test case 2: empty list list:[]
+        result = remove_empty_strings([])
+        self.assertEqual(result, [])
+
+    def test_remove_characters_keep_integer(self):
+        # Test case 1: string  with numbers str:"qw 23 dw ddw 55"
+        result = remove_characters_keep_integers("qw 23 dw ddw 55")
+        self.assertEqual(result, "2355")
+
+        # Test case 2: string  with numbers within str str:"qw23 dw ddw 55"
+        result = remove_characters_keep_integers("qw23 dw ddw 55")
+        self.assertEqual(result, "2355")
+
+        # Test case 3: empty str:""
+        result = remove_characters_keep_integers("")
+        self.assertEqual(result, "")
+
+    def test_find_combination_of_alphabets_numbers(self):
+        # Test case 1: string contains alphanumeric str:"str1 string in the3"
+        result = find_combination_of_alphabets_numbers("str1 string in the3")
+        self.assertEqual(result, ["str1", "the3"])
+
+        # Test case 2: string contains alphanumeric and number str:"str1 string in the3 33"
+        result = find_combination_of_alphabets_numbers("str1 string in the3 33")
+        self.assertEqual(result, ["str1", "the3"])
+
+        # Test case 3: string contains no alphanumeric str:"str string in the"
+        result = find_combination_of_alphabets_numbers("str string in the 33")
+        self.assertEqual(len(result), 0)
+
+        # Test case 4: empty str:""
+        result = find_combination_of_alphabets_numbers("")
+        self.assertEqual(len(result), 0)
+
+    def test_replace_symbols(self):
+        # Test case 1: basic str: $tring w!th speci@l chara&cters
+        result = replace_symbols("$tring w!th speci@l chara&cters","#")
+        self.assertEqual(result, "#tring w#th speci#l chara#cters")
+
+        # Test case 2: no special char str: String with special characters
+        result = replace_symbols("String with special characters","#")
+        self.assertEqual(result, "String with special characters")
+
+        # Test case 3: empty str: ""
+        result = replace_symbols("","#")
+        self.assertEqual(result, "")
+
         
 if __name__ == '__main__':
     unittest.main()
